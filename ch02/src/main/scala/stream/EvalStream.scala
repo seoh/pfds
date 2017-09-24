51225ce7c4c7e7982109b2dc0c3fa5dfe4380981
seoh <devthewild@gmail.com>
2017-09-24 17:15:32 +0900

    ch02: implement stream using cats.Eval

---
+++ ch02/src/main/scala/stream/EvalStream.scala
@@ -0,0 +1,20 @@
+package stream
+
+import cats.Eval
+
+object EvalStream extends App {
+  implicit val evalStream: StreamOps[Eval] = new StreamOps[Eval] {
+    override def delay[T](exp: => StreamCell[Eval, T]): Stream[Eval, T] =
+      Stream(Eval.later(exp))
+
+    override def force[T](stream: Stream[Eval, T]): StreamCell[Eval, T] =
+      stream.suspend.value
+  }
+
+  val s: Stream[Eval, Int] = Stream.fromList((0 until 10).toList)
+  val e: Stream[Eval, Int] = delay(Nil)
+
+  assert(e.take(2).toString == "Stream()",      "empty.take(n) should be Nil")
+  assert(s.take(0).toString == "Stream()",      "nonEmpty.take(0) should be Nil")
+  assert(s.take(2).toString == "Stream(0, 1)",  "nonEmpty.take(n) when n>0, should not be Nil")
+}
